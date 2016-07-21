db428c1bd733d88ee3ee0409d816a83b1a939dce
Anastasiya Karimova <akarimova@yandex-team.ru>
2016-07-21 12:37:01 +0300

    initial commit

---
+++ app/src/main/java/akarimova/bluetoothlibrary/MainActivity.java
@@ -0,0 +1,13 @@
+package akarimova.bluetoothlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
